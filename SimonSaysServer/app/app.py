82b295f4d4eea276712c8376bbc390a514fb75dc
Khalid Mahyou <khal1430@gmail.com>
2019-10-26 17:19:50 +0200

    Communicaition@

--- SimonSaysServer/app/app.py
+++ SimonSaysServer/app/app.py
@@ -2,10 +2,35 @@
 
 from sense_hat import SenseHat
 from time import sleep
+import paho.mqtt.client as mqtt
 
 sense = SenseHat()
 event = sense.stick.wait_for_event()
 print("The joystick was {} {}".format(event.action, event.direction))
 
 # Send event to server
+
+# broker server
+broker_address = "10.10.169.177"
+
+def on_message(client, userdata, message):
+    print("message received ", str(message.payload.decode("utf-8")))
+    print("message topic=", message.topic)
+
+print("creating new instance")
+client = mqtt.Client("P1")   #create new instance
+client.on_message=on_message #attach function to callback
+
+print("connecting to broker")
+client.connect(broker_address) #connect to broker
+
+client.loop_start() #start the loop
+print("Subscribing to topic", "server/data")
+client.subscribe("server/data")
+
+print("Publishing message to topic", "server/data")
+client.publish("server/data", "This is message sent from the servr")
+
+time.sleep(4) # wait
+client.loop_stop() #stop the loop
 
