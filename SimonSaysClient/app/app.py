27a21e392456a3a7b6ac63a2a9230c46afd344cb
buenassss <nuria.fernandezm@gmail.com>
2019-10-26 16:22:24 +0200

    Manage led when user uses joystick

--- SimonSaysClient/app/app.py
+++ SimonSaysClient/app/app.py
@@ -4,23 +4,63 @@ from sense_hat import SenseHat
 from time import sleep
 import socket, sys
 
+sense = SenseHat()
+
+def turnOffLedMatrix():
+	O = [0, 0, 0]  # black
+
+	screen = [
+	O, O, O, O, O, O, O, O,
+	O, O, O, O, O, O, O, O,
+	O, O, O, O, O, O, O, O,
+	O, O, O, O, O, O, O, O,
+	O, O, O, O, O, O, O, O,
+	O, O, O, O, O, O, O, O,
+	O, O, O, O, O, O, O, O,
+	O, O, O, O, O, O, O, O
+	]
+
+	sense.set_pixels(screen)
+
+def turnOnLedMatrix():
+	
+	O = [255, 255, 255]  # White
+
+	screen = [
+	O, O, O, O, O, O, O, O,
+	O, O, O, O, O, O, O, O,
+	O, O, O, O, O, O, O, O,
+	O, O, O, O, O, O, O, O,
+	O, O, O, O, O, O, O, O,
+	O, O, O, O, O, O, O, O,
+	O, O, O, O, O, O, O, O,
+	O, O, O, O, O, O, O, O
+	]
+
+	sense.set_pixels(screen)
+
 ip = socket.gethostbyname(socket.gethostname())
 print("Hostname: ", socket.gethostname())
 print ("IP: ", ip)
-
-sense = SenseHat()
-event = sense.stick.wait_for_event()
-print("The joystick was {} {}".format(event.action, event.direction))
 
 configPath = './conf'
 configFile = open(configPath, 'r')
 hostname = configFile.readline()
 
 if hostname == socket.gethostname():
-    print("SERVER")
-elif 
-    print("CLIENT -> exit")
+    print("SERVER -> exit")
     sys.exit(0)
+elif 
+    print("CLIENT")
+
+    #wait message from server
+    
+	while True:
+        for event in sense.stick.get_events():
+            turnOnLedMatrix()	
+            turnOffLedMatrix()
+
+	        #Send message to server
 
 
 # Send event to server
