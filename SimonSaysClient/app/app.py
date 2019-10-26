4d6284734abcf01fc6c3f9f1316a72bef7be9cc2
buenassss <nuria.fernandezm@gmail.com>
2019-10-26 16:25:07 +0200

    solve syntax error

--- SimonSaysClient/app/app.py
+++ SimonSaysClient/app/app.py
@@ -50,7 +50,7 @@ hostname = configFile.readline()
 if hostname == socket.gethostname():
     print("SERVER -> exit")
     sys.exit(0)
-elif 
+elif:
     print("CLIENT")
 
     #wait message from server
