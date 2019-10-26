8008547a6c88289a6b55424249a0ab3a0e46865c
Carlos Leal <carlos.leal@tomtom.com>
2019-10-26 16:03:31 +0200

    IP

--- SimonSaysClient/app/app.py
+++ SimonSaysClient/app/app.py
@@ -4,14 +4,16 @@ from sense_hat import SenseHat
 from time import sleep
 import socket
 
+ip = socket.gethostbyname(socket.gethostname())
+print("Hostname: ", socket.gethostname())
+print ("IP: ", ip)
+
 sense = SenseHat()
 event = sense.stick.wait_for_event()
 print("The joystick was {} {}".format(event.action, event.direction))
 
 configPath = './conf'
 configFile = open(configPath, 'r')
-ip = socket.gethostbyname(socket.gethostname())
-print ("IP: ", ip)
 
 # Send event to server
 
