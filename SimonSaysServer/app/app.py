242061d2f07919493e3a840ba80dadb61b0deb90
buenassss <nuria.fernandezm@gmail.com>
2019-10-27 22:24:35 +0100

    nose

--- SimonSaysServer/app/app.py
+++ SimonSaysServer/app/app.py
@@ -1,23 +1,18 @@
 # Server: SimonSaysServer contains the logic to execute simon logic.
 
 from sense_hat import SenseHat
-import time
+from time import sleep
 import paho.mqtt.client as mqtt
-#import socket, sys
+import socket, sys
 
-#ip = socket.gethostbyname(socket.gethostname())
-#print("Hostname: ", socket.gethostname())
-#print ("IP: ", ip)
+sense = SenseHat()
+event = sense.stick.wait_for_event()
+print("The joystick was {} {}".format(event.action, event.direction))
 
-#sense = SenseHat()
-#event = sense.stick.wait_for_event()
-#print("The joystick was {} {}".format(event.action, event.direction))
+configPath = './conf'
+configFile = open(configPath, 'r')
+hostname = configFile.readline()
 
-#configPath = './conf'
-#configFile = open(configPath, 'r')
-#hostname = configFile.readline()
-
-'''
 print ("Socket hostname: ", socket.gethostname())
 print ("Config hostname: ", hostname)
 if hostname == socket.gethostname():
@@ -25,29 +20,28 @@ if hostname == socket.gethostname():
 else:
     print("CLIENT -> exit")
     sys.exit(0)
-'''
 
 # broker server
 broker_address = "10.10.169.177"
-broker_port = 1883
 
 def on_message(client, userdata, message):
     print("message received ", str(message.payload.decode("utf-8")))
     print("message topic=", message.topic)
 
 print("creating new instance")
-client = mqtt.Client("P1")   #create new instance
+client = mqtt.Client(hostname)   #create new instance
 client.on_message=on_message #attach function to callback
 
 print("connecting to broker")
-client.connect(broker_address, port=broker_port) #connect to broker
+client.connect(broker_address) #connect to broker
 
 client.loop_start() #start the loop
 print("Subscribing to topic", "server/data")
-client.subscribe("server/data")
+client.subscribe("server/sequence")
 
-print("Publishing message to topic", "server/data")
-client.publish("server/data", "This is message sent from the servr")
+while True:
+    print("Publishing message to topic", "server/data")
+    client.publish("server/sequence", "Nodo1")
 
 time.sleep(4) # wait
 client.loop_stop() #stop the loop
